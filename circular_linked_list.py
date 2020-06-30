1a34e259c268b0cbc9f0c4ee1da03b63d95f1a9a
shubhamshewdikar <shubham.shewdikar@gmail.com>
2020-06-30 17:59:37 +0530

    doubly and singly linked list is added

---
+++ circular_linked_list.py
@@ -0,0 +1,52 @@
+class node():
+    def __init__(self, data):
+        self.next = None
+        self.prev = None
+        self.data = data
+
+class circularLinkList:
+    def __init__(self):
+        self.head = None
+    
+    def insertAtFirst(self, data):
+        new_node = node(data)
+        temp = self.head
+        new_node.next = self.head
+
+        if self.head is not None:
+            while temp.next != self.head:
+                temp = temp.next
+            temp.next = new_node
+        else:
+            new_node.next = new_node
+        
+        self.head = new_node
+
+    def deleteAtFirst(self, data):
+        if self.head is None:
+            print("List is empty")
+        elif self.head.next is None:
+            self.head = None
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            
+            temp.next = self.head.next
+            self.head = temp.next
+
+    def displayList(self):
+        if self.head is None:
+            print("List is empty")
+        else:
+            temp = self.head
+            while True:
+                print(temp.data)
+                temp = temp.next
+                if temp == self.head:
+                    break
+
+obj = circularLinkList()
+obj.insertAtFirst(2)
+obj.displayList()
+# obj.insertAtFirst(2)
---
+++ doubly_linked_list.py
@@ -0,0 +1,84 @@
+class node():
+    def __init__(self, data):
+        self.next = None
+        self.prev = None
+        self.data = data
+
+class doublyLinkList:
+    def __init__(self):
+        self.head = None
+
+    def insertAtBegining(self, data):
+        new_node = node(data)
+
+        if self.head is not None:
+            self.head.prev = new_node
+            
+        self.head = new_node
+
+    def insertAtEnd(self, data):
+        new_node = node(data)
+
+        if self.head is None:
+            new_node.prev = None 
+            self.head = new_node
+            return
+
+        temp = self.head
+        while temp.next is not None:
+            temp = temp.next
+        
+        temp.next = new_node
+        new_node.prev = temp
+
+    def insertAfter(self, prev, data):
+        new_node = node(data)
+
+        if prev is None:
+            print("invalid")
+        
+        new_node.next = prev.next
+        new_node.prev = prev
+        prev.next = new_node
+
+        if new_node.next is not None:
+            new_node.next.prev = new_node
+
+    def displayList(self, last=None):
+        if self.head is None:
+            print("List is Empty")
+        else:
+            node = self.head
+            print("\nTraversal in Forward direction")
+            while node is not None:
+                print(node.data, end=" ")
+                last = node
+                node = node.next
+            
+            print("\nTraversal in Reverse direction")
+            while last is not None:
+                print(last.data, end=" ")
+                last = last.prev
+
+    def displayListFromAnyNode(self, node=None, last=None):
+        if self.head is None:
+            print("List is Empty")
+        
+        print("\nTraversal in Forward direction")
+        while node is not None:
+            print(node.data, end=" ")
+            last = node
+            node = node.next
+        print("\nTraversal in Reverse direction")
+        while last is not None:
+            print(last.data, end=" ")
+            last = last.prev
+
+
+obj = doublyLinkList()
+
+# obj.displayList()
+obj.insertAtBegining(0)
+obj.insertAtEnd(10)
+obj.insertAfter(obj.head,5)
+obj.displayListFromAnyNode(obj.head)
